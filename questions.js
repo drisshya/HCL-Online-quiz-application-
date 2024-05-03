f79da55e84ea3829a51d7797ac890f8185de4d22
Drisshya S <drisshyasunil1224@gmail.com>
2024-05-03 19:28:21 +0530

    first commit

---
+++ questions.js
@@ -0,0 +1,244 @@
+
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the following attributes is used to add link to any element?",
+    imgSrc: "Stuffs/img/link.jpeg",
+    answer: "href",
+    options: [
+      "link",
+      "href",
+      "ref",
+      "newref"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does HTML stand for?",
+    imgSrc: "Stuffs/img/html.jpeg",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Markup Language",
+      "Hyper Text Preprocessor",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    imgSrc: "Stuffs/img/php.jpg",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does CSS stand for?",
+    imgSrc: "Stuffs/img/css.jpeg",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which feature of OOP indicates code reusability?",
+    imgSrc: "Stuffs/img/oops.jpg",
+    answer: "Inheritance",
+    options: [
+        "Abstraction",
+        "Polymorphism",
+        "Inheritance", 
+        "Encapsulation"
+    ]
+},
+{
+    numb: 6,
+    question: "Which of the following is not a valid C variable name?",
+    imgSrc: "Stuffs/img/variable.jpg",
+    answer: "int $main",
+    options: [
+         "float rate",
+         "int number",
+         "int variable_count", 
+         "int $main"
+    ]
+},
+{
+    numb: 7,
+    question: "In the 'C' language local variables that can retain their value between funcion calls are called as",
+    imgSrc: "Stuffs/img/local.jpeg",
+    answer: "static",
+    options: [
+         "register",
+         "static",
+         "auto",
+         "extern"
+    ]
+},
+{
+        numb: 8,
+        question: "The process of an object controlling outside access to its internal data is known as",
+        imgSrc: "Stuffs/img/encapsulation.png",
+        answer: "Encapsulation",
+        options: [
+          "Encapsulation",
+          "Polymorphism",
+          "Inheritence",
+          "Abstraction"
+        ]
+    },
+{
+        numb: 9,
+        question: "Which of the following is not a computer language?",
+        imgSrc: "Stuffs/img/program.png",
+        answer: "Linux",
+        options: [
+            "C++",
+            "Python",
+            "Linux", 
+            "Java"
+        ]
+    },
+{
+        numb: 10,
+        question: "Which data type value can an int variable not store?",
+        imgSrc: "Stuffs/img/int.jpg",
+        answer: "2147483648",
+        options: [
+            "-32768",
+            "2147483648",
+            "1000000",
+            "-2147483648"
+        ]
+    },
+{
+        numb: 11,
+        question: "What does SQL stand for?",
+        imgSrc: "Stuffs/img/sql.jpeg",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+{
+        numb: 12,
+        question: "What does XML stand for?",
+        imgSrc: "Stuffs/img/xml.jpg",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+{
+        numb: 13,
+        question: "Which data structure is used for implementing recursion?",
+        imgSrc: "Stuffs/img/recursion.jpeg",
+        answer: "Stack",
+        options: [
+            "Queue",
+            "List",
+            "Stack",
+            "Array"
+        ]
+    },
+{
+        numb: 14,
+        question: "The prefix form of A-B/(C*D^E) is?",
+        imgSrc: "Stuffs/img/expression.png",
+        answer: "-A/B*C^DE",
+        options: [
+            "-A/BC*^DE",
+            "-A/B*C^DE",
+            "-ABCD*^DE",
+            "-/*^ACBDE"
+        ]
+    },
+{
+        numb: 15,
+        question: "What is the maximum height of an AVL tree with p nodes?",
+        imgSrc: "Stuffs/img/tree.png",
+        answer: "log(p)",
+        options: [
+            "p",
+            "log(p)",
+            "log(p)/2",
+            "p/2"
+        ]
+    },
+{
+        numb: 16,
+        question: "Which of the following is used for comments in C++?",
+        imgSrc: "Stuffs/img/comments.jpg",
+        answer: "both //comment or /*comment*/",
+        options: [
+            "/*comment*/",
+            "//comment*/",
+            "//comment",
+            "both //comment or /*comment*/"
+        ]
+    },
+{
+        numb: 17,
+        question: "Which of the following type is provided by C++ but not C?",
+        imgSrc: "Stuffs/img/datatype.jpg",
+        answer: "bool",
+        options: [
+            "double",
+            "bool",
+            "float",
+            "int"
+        ]
+    },
+{
+        numb: 18,
+        question: "Which of the following is not javascript data types?",
+        imgSrc: "Stuffs/img/datatypes.png",
+        answer: "All of the mentioned",
+        options: [
+          "Null type",
+          "Undefined type",
+          "Number type",
+          "All of the mentioned"
+        ]
+    },
+{
+        numb: 19,
+        question: "Who is the father of C language?",
+        imgSrc: "Stuffs/img/Clang.png",
+        answer: "Dennis Ritchie",
+        options: [
+          "Steve Jobs",
+          "James Gosling",
+          "Dennis Ritchie",
+          "Rasmus Lerdorf"
+        ]
+    },
+{
+        numb: 20,
+        question: "Functions in C language are always",
+        imgSrc: "Stuffs/img/function.png",
+        answer: "External",
+        options: [
+          "Internal",
+          "External",
+          "Both Internal and External",
+          "External and Internal are not valid terms for functions"
+        ]
+    },
+    
+];
